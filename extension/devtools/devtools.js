e43d8ee9524005062066c1db28a3043ca48ab7d9
davidjeba <davidjeba.d@gmail.com>
2026-09-19 18:59:33 +0530

    feat(extension): bundle high-res 16/48/128px PNG icons in manifest and releases

--- extension/devtools/devtools.js
+++ extension/devtools/devtools.js
@@ -1,7 +1,6 @@
-// Register the HMLR DevTools Panel in browser developer tools
 chrome.devtools.panels.create(
   "⚡ HMLR",
-  "",
+  "../icons/icon16.png",
   "devtools/panel.html",
   (panel) => {
     console.log("HMLR DevTools Panel Registered Successfully.");
